011ef261f9a83ef4a8a80b721499b374aa09de52
Carolina05-ai <carolinaigonzalezbetancourt@gmail.com>
2025-07-31 12:26:01 -0500

    repositorio js version 1

---
+++ 1-sintaxis-variables/app.js
@@ -0,0 +1,29 @@
+//el alert muestra una ventana emergente en el navegador
+alert ("Hola Mundo");
+//el prompt muestra una caja para ingresar informacion desde el navegador
+prompt ("Dime Tu Nombre");
+
+//variable declarada con let
+let personaje = "Tom"
+//crear variable puntaje y asignar valor de 0
+//los valores numericos van sin comillas
+let puntaje = 0
+//crear variable shots y asignar valor de 3
+let shots = 3
+   recargar = 2
+
+console.log("total balas:" ,shots + recargar)
+
+//el usuario capturo 4 patos
+puntaje += 4
+//el usuario gasto 2 balas
+shots -= 2
+//el usuario recargo su arma
+recargar += 1
+recargar++ //++ permite sumar de a uno
+recargar++ //++ permite sumar de a uno
+
+
+console.log("puntaje:" ,puntaje) //4
+console.log("shots:" ,shots) //1 
+console.log("recargar:" ,recargar) //3
