052d9129f476036d3f17e2b366b4ac52a1365642
Carolina05-ai <carolinaigonzalezbetancourt@gmail.com>
2025-08-05 12:54:46 -0500

    3_condicionales_actualizado

--- 3_condicionales/app2.js
+++ 3_condicionales/app2.js
@@ -1,13 +1,13 @@
 //EJERCICIO 1
 
 //nombre del usuario
-let nombre = prompt("¿Cuál es tu nombre?");
+let nombre = prompt("¿Cuál es tu nombre?").toLowerCase().trim();
 
 //edad del usuario y la convertimos a número
 let edad = Number(prompt("¿Cuántos años tienes?"));
 
 //nombre es "mario" o "carlos"
-if (nombre.toLowerCase() === "mario" || nombre.toLowerCase() === "carlos") {
+if (nombre === "mario" || nombre === "carlos") {
   // Si se llama Mario o Carlos, puede entrar a zona VIP
   alert("Puedes ingresar. Bienvenido a la zona VIP");
 
@@ -18,24 +18,27 @@ if (nombre.toLowerCase() === "mario" || nombre.toLowerCase() === "carlos") {
 } else {
   // Si no cumple ninguna de las condiciones anteriores, no puede entrar
   alert("No puedes ingresar a la discoteca");
-}
+} 
 
 //EJERCICIO 2
 
- // Pedimos la estatura en cm
+//nombre del usuario
+let nombre1= prompt("¿Cuál es tu nombre?").toLowerCase().trim();
+
+ // Pedimos la estatura en cm 
 let estatura = Number(prompt("¿Cuál es tu estatura en cm?"));
 
 // Pedimos la velocidad en segundos (100 metros planos)
-let velocidad = Number(prompt("¿Cuál es tu velocidad en segundos para 100 metros?"));
+let velocidad = Number(prompt("¿Cuál es tu velocidad en hora para 100KM?"));
 
 // Pedimos la edad
 let edad2 = Number(prompt("¿Cuántos años tienes?"));
 
 // Verificamos si cumple con los requisitos de estatura y velocidad
-if (estatura >= 170 && velocidad <= 27) {
-
+if (estatura >= 170 && velocidad >= 12 ) {
+ 
   // Si cumple, ahora verificamos la edad para saber a qué grupo va
-  if (edad3 < 18) {
+  if (edad2 < 18) {
     alert("Puedes ingresar a las divisiones menores del equipo");
   } else {
     alert("Puedes ingresar al equipo profesional");
